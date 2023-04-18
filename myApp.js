36c7df4ac3addbcb6a60a9674327fad50a4b7ecb
Saghar Hosseinmardi <saghar.h.mardi@gmail.com>
2023-04-18 18:35:15 +0100

    level 3

--- myApp.js
+++ myApp.js
@@ -1,14 +1,22 @@
 let express = require("express");
 let app = express();
-
-console.log("Hello world");
-
+// level 1
+// console.log("Hello world");
+// level 2
 // app.get("/", (req, res) => {
 //   res.send("Hello Express");
 // });
 
-app.get("/", (req, res) => {
-  res.send(__dirname + "/views/index.html");
+// level 3
+// const absolutePath = __dirname + "/views/index.html";
+
+// app.get("/", (req, res) => {
+
+//   res.sendFile(absolutePath)
+// });
+
+app.get("/",(req, res) => {
+  res.sendFile(__dirname + "/views/index.html")
 });
 
 module.exports = app;
