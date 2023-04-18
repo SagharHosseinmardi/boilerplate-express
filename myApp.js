a4bbe5d319d8d7e93c49218fa98f3f5094d96b14
Saghar Hosseinmardi <saghar.h.mardi@gmail.com>
2023-04-18 18:20:50 +0100

    level3

--- myApp.js
+++ myApp.js
@@ -1,41 +1,14 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 
 console.log("Hello world");
 
+// app.get("/", (req, res) => {
+//   res.send("Hello Express");
+// });
+
 app.get("/", (req, res) => {
-  res.send("Hello Express");
-}); 
+  res.send(__dirname + "/views/index.html");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
